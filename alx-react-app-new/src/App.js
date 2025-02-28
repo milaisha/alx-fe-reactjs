31110490beb23fef852509f2be5b8c12b3529cd9
Aisha <milaisha098@gmail.com>
2025-02-28 17:50:31 +0300

    Added Counter component

--- alx-react-app-new/src/App.js
+++ alx-react-app-new/src/App.js
@@ -1,16 +1,12 @@
-import Header from "./components/Header.jsx";
-import MainContent from "./components/MainContent.jsx";
-import Footer from "./components/Footer.jsx";
-import UserProfile from "./components/UserProfile.jsx";
+import Counter from './components/Counter';
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
+
 export default App;
+
