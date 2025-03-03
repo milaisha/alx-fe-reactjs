7464585629606b2fb18e0d4ad7d31746519958cf
Aisha <milaisha098@gmail.com>
2025-03-03 22:21:03 +0300

    Organized repository structure as per requirements

--- my-company/App.jsx
+++ my-company/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import Navbar from './components/Navbar';
+import Home from './components/Home.jsx';
+import About from './components/About.jsx';
+import Services from './components/Services.jsx';
+import Contact from './components/Contact.jsx';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
